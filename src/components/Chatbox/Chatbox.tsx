2de8f3b3502c3909ea9da948e8b70a5802b06c1b
suhana salma <suhanasalma800@gmail.com>
2024-01-11 14:30:30 +0600

    chatbox designed

--- src/components/Chatbox/Chatbox.tsx
+++ src/components/Chatbox/Chatbox.tsx
@@ -30,15 +30,16 @@ const Chatbox = ({ messages }: Messages) => {
         >
             <div className={`flex flex-col items-end justify-end h-full `}>
                 {messages?.map((message, index) => (
-                    <div className={`flex items-start gap-5 my-2 ${message.sender === user.email ? "self-end" : "self-start"
+                    <div className={`flex items-start  gap-5 my-2 w-6/12 ${message.sender === user.email ? " justify-end self-end" : "self-start justify-start"
                         }`}>
-                        <img src={message?.img ? message.img : userImage} className="w-8 h-8 rounded-full object-cover object-top" alt="" />
+                       <div className="flex justify-start gap-5">
+                       <img src={message?.img ? message.img : userImage} className="w-8 h-8 rounded-full object-cover object-top" alt="" />
                         <div
-                            className={`py-1 px-4 rounded-lg  flex  items-end justify-between gap-5 ${message.sender === user.email ? " bg-green" : "bg-white"
+                            className={`py-1 px-4 rounded-md  ${message.sender === user.email ? " bg-green" : "bg-white"
                                 }`}
                             key={index}
                         >
-                            <p>{message.content}</p>
+                            <p className="">{message.content}</p>
                             <div className="text-[10px] text-slate flex items-center justify-end ">
                                 <p>
                                     {message.timestamp.toLocaleTimeString(
@@ -57,6 +58,7 @@ const Chatbox = ({ messages }: Messages) => {
                                 </p>
                             </div>
                         </div>
+                       </div>
                     </div>
                 ))}
             </div>
