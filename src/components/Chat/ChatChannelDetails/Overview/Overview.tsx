960c9a6688bc3f068821e192a776bc18126b5a3f
suhana salma <suhanasalma800@gmail.com>
2024-02-15 18:03:54 +0600

    overview page running

--- src/components/Chat/ChatChannelDetails/Overview/Overview.tsx
+++ src/components/Chat/ChatChannelDetails/Overview/Overview.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+const moment = require('moment-timezone');
 
 
 interface OverviewProps {
@@ -7,21 +8,35 @@ interface OverviewProps {
     name: string;
 }
 const Overview = ({ overviewDetails, img, name }: OverviewProps) => {
+    let time = moment(overviewDetails.createdAt).format("YYYY-MM-DD hh:mm A");
+    console.log("time", time);
     return (
         <div className='px-5 py-8'>
-            
+
             <img className="object-cover h-20 w-20 rounded-full " src={img} alt="user" />
             <p className='font-bold text-md'>{name}</p>
             {
-                !overviewDetails.group_type &&  <p className='text-slate'>{overviewDetails.email}</p>
-            }
-            {
-                overviewDetails.group_type!=="one-to-one" &&  <div>
-                    <p className='text-slate'>Created</p>
-                    <p className='text-slate'>{overviewDetails.created_at}</p>
+                !overviewDetails.group_type && <div>
+                    <p className='text-slate'>{overviewDetails.email}</p>
+                    <div>
+                        <button>Block</button>
+                        <button>Report contact</button>
+                    </div>
                 </div>
             }
-           
+            {
+                overviewDetails.group_type && overviewDetails.group_type !== "one-to-one" && <div>
+                    <div>
+                        <p className='text-slate'>Created</p>
+                        <p className='text-slate'>{time}</p>
+                    </div>
+                    <div>
+                        <button>Exit group</button>
+                        <button>Report group</button>
+                    </div>
+                </div>
+            }
+
         </div>
     );
 };
