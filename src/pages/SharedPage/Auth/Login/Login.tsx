e1af3d1f607ddecbfc366a49a2be71ce074da687
suhana salma <suhanasalma800@gmail.com>
2024-02-20 10:48:37 +0600

    login page done

--- src/pages/SharedPage/Auth/Login/Login.tsx
+++ src/pages/SharedPage/Auth/Login/Login.tsx
@@ -29,11 +29,14 @@ const Login = ({ registerPage }: LoginProps) => {
     const [login, { data, error, isLoading }] = useLoginMutation();
     const [openEmailLabel,setOpenEmailLabel] = useState(false);
     const [openPassLabel,setOpenPassLabel] = useState(false);
+    const [type,setType] = useState("password");
 
 
 
     const onSubmit: SubmitHandler<IFormInput> = async (data) => {
         try {
+
+            console.log("data",data);
             const response = await login(data);
             if ('data' in response && response.data) {
 
@@ -48,35 +51,19 @@ const Login = ({ registerPage }: LoginProps) => {
             console.error("Fetch error:", error);
         };
     }
+
+    const handlePasswordTypeToggle = () =>{
+
+        if(type==="password"){
+            setType('text');
+            setSeePassword(true);
+        }else{
+            setType('password');
+            setSeePassword(false);
+        }
+
+    }
     return (
-        // <section className='h-screen overflow-auto bg-soft-gray'>
-        //     <article className='w-9/12 mx-auto my-10 shadow-2xl bg-white flex flex-col md:flex-row  justify-between p-10'>
-        //         <div className='md:w-5/12 xl:w-6/12 bg-teal-green rounded-br-full min-h-fit max-h-96'>
-        //             <img src={log} alt="" className='h-full' />
-        //         </div>
-        //         <div className='md:w-6/12 xl:w-4/12 p-5  min-h-fit max-h-96 overflow-auto bg-slate shadow-md m-5  rounded-md'>
-        //             <p className='font-bold my-2 text-white text-center'>LOG IN HERE</p>
-        //             <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-5'>
-
-        //                 <div className='relative'>
-        //                     <input type='email' className='w-full rounded-md outline-none py-1 px-2' placeholder='email' {...register("email", { required: true })} />
-        //                     <MdOutlineAlternateEmail className=' absolute bottom-2 right-2 text-lg text-teal-green-dark' />
-        //                     {errors.email && <p className='text-xs text-red'>email is required.</p>}
-        //                 </div>
-        //                 <div className='relative'>
-        //                     <input type='text' className='w-full rounded-md outline-none py-1 px-2' placeholder='password' {...register("password", { required: true, maxLength: 20 })} />
-        //                     <RiLockPasswordLine className=' absolute bottom-2 right-2 text-lg text-teal-green-dark' />
-        //                     {errors.password && <p className='text-xs text-red'>email is required.</p>}
-        //                 </div>
-
-
-        //                 <p className='text-light-gray text-xs text-center'>Don't have a account already? <Link to='/register' className='text-green underline cursor-pointer'>Register</Link></p>
-
-        //                 <input className='w-6/12 mx-auto p-2 bg-teal-green rounded-md text-white' type="submit" />
-        //             </form>
-        //         </div>
-        //     </article>
-        // </section >
         <div className={`absolute w-[85%] duration-500 ease-in-out ${!registerPage ? " left-[32px]" : "left-[-450px]"}`} id="login">
             <div className="top-header">
                 <h3>Hello, Again!</h3>
@@ -84,17 +71,17 @@ const Login = ({ registerPage }: LoginProps) => {
             </div>
             <form onSubmit={handleSubmit(onSubmit)} className="input-group">
                 <div className="input-field relative" >
-                    <input onClick={()=>setOpenEmailLabel(true)} type="email" className="input-box" id="logEmail" required  {...register("email", { required: true })} />
+                    <input onClick={()=>setOpenEmailLabel(true)} type="email" className="input-box" id="logEmail"   {...register("email", { required: true })} />
                     <label className={`absolute  ${openEmailLabel ? "top-[2px] text-[10px] font-medium text-[#c12828]":"top-3"}`} >Email address</label>
                     {errors.email && <p className='text-sm text-[#850101] w-fit mt-2 rounded-sm'>email is required.</p>}
                 </div>
                 <div className="input-field relative">
-                    <input onClick={()=>setOpenPassLabel(true)} type="password" className="input-box" id="logPassword" required {...register("password", { required: true, maxLength: 20 })} />
+                    <input onClick={()=>setOpenPassLabel(true)} type={type} className="input-box" id="logPassword"  {...register("password", { required: true, maxLength: 20 })} />
                     <label className={`absolute  ${openPassLabel ? "top-[2px] text-[10px] font-medium text-[#c12828]":"top-3"}`}>Password</label>
                     <div className="eye-area">
                         <div className="eye-box" >
-                           {seePassword? <FaEye onClick={()=>setSeePassword(false)} className="i" />:
-                            <FaEyeSlash onClick={()=>setSeePassword(true)} className="i"/>}
+                           {seePassword? <FaEye onClick={handlePasswordTypeToggle} className="i" />:
+                            <FaEyeSlash onClick={handlePasswordTypeToggle} className="i"/>}
                         </div>
                     </div>
                     {errors.password && <p className='text-sm text-[#850101] w-fit mt-2 rounded-sm'>password is required.</p>}
@@ -104,7 +91,7 @@ const Login = ({ registerPage }: LoginProps) => {
                     <input type="submit" className="input-submit" value="Sign In" required />
                 </div>
                 <div className="forgot">
-                    <a href="#">Forgot password?</a>
+                    <Link className='text-link-blue' to="/">Forgot password?</Link>
                 </div>
 
             </form>
